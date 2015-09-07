fa5ae5fd84814173391d0ba2027092a5dc5e1a0d
Ezard <ezard.ben@gmail.com>
2015-09-07 20:01:36 +0100

    Changed Rigidbody so that it uses a 2 dimensional velocity field now, as opposed to a y-axis only gravity field

--- com/ezardlabs/dethsquare/Rigidbody.java
+++ com/ezardlabs/dethsquare/Rigidbody.java
@@ -1,13 +1,13 @@
 package com.ezardlabs.dethsquare;
 
 public final class Rigidbody extends Script {
-	public float gravity = 0.9375f;
+    public Vector2 velocity = new Vector2(0, 0.9375f);
 
 	@Override
 	public void update() {
-		gravity += 1.25f;
-		if (gravity > 78.125f) gravity = 78.125f;
+		velocity.y += 1.25f;
+		if (velocity.y > 78.125f) velocity.y = 78.125f;
 
-		transform.translate(0, gravity);
+		transform.translate(velocity.x, velocity.y);
 	}
 }
