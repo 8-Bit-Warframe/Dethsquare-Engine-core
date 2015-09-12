ad8bdb5195d57b67bde04b4e4ba662c8962e41dd
Ezard <ezard.ben@gmail.com>
2015-09-12 01:46:56 +0100

    Added a gravity field to Rigidbody

--- com/ezardlabs/dethsquare/Rigidbody.java
+++ com/ezardlabs/dethsquare/Rigidbody.java
@@ -2,10 +2,11 @@ package com.ezardlabs.dethsquare;
 
 public final class Rigidbody extends Script {
     public Vector2 velocity = new Vector2(0, 0.9375f);
+	public float gravity = 1.25f;
 
 	@Override
 	public void update() {
-		velocity.y += 1.25f;
+		velocity.y += gravity;
 		if (velocity.y > 78.125f) velocity.y = 78.125f;
 
 		transform.translate(velocity.x, velocity.y);
