aa842eb48fa4fea592e715394ce60fee962270ea
Ezard <ezard.ben@gmail.com>
2016-07-05 14:54:43 +0100

    Add ability to get the current level and current level's name

--- com/ezardlabs/dethsquare/LevelManager.java
+++ com/ezardlabs/dethsquare/LevelManager.java
@@ -4,17 +4,29 @@ import java.util.HashMap;
 
 public class LevelManager {
 	private static HashMap<String, Level> levels = new HashMap<>();
+	private static String currentLevelName;
 
 	public static void registerLevel(String name, Level level) {
 		levels.put(name, level);
 	}
 
 	public static void loadLevel(String name) {
+		currentLevelName = name;
+
 		Level level = levels.get(name);
 		level.onLoad();
 
 		GameObject.startAll();
 		Renderer.init();
 		Collider.init();
+	}
+
+	public static Level getCurrentLevel() {
+		if (currentLevelName == null) return null;
+		else return levels.get(currentLevelName);
+	}
+
+	public static String getCurrentLevelName() {
+		return currentLevelName;
 	}
 }
