4b4fdeae14d0d48a01caee6616c6cda5992e8d65
Ezard <ezard.ben@gmail.com>
2016-10-18 02:12:22 +0100

    Add first iteration of parent-child relationship between Transforms

--- src/main/java/com/ezardlabs/dethsquare/Transform.java
+++ src/main/java/com/ezardlabs/dethsquare/Transform.java
@@ -1,10 +1,50 @@
 package com.ezardlabs.dethsquare;
 
+import java.util.ArrayList;
+
 /**
  * {@link Component} that contains the position of a {@link GameObject}
  */
 public final class Transform extends Component {
 	public Vector2 position = new Vector2();
+
+	private Transform parent;
+	private ArrayList<Transform> children = new ArrayList<>();
+
+	Transform() {
+		position.setVector2ChangeListener((xDiff, yDiff) -> {
+			for (int i = 0; i < children.size(); i++) {
+				children.get(i).position.x += xDiff;
+				children.get(i).position.y += yDiff;
+			}
+		});
+	}
+
+	public void setParent(Transform parent) {
+		setParent(parent, true);
+	}
+
+	private void setParent(Transform parent, boolean addChild) {
+		this.parent = parent;
+		if (addChild) parent.addChild(this, false);
+	}
+
+	public Transform getParent() {
+		return parent;
+	}
+
+	public void addChild(Transform child) {
+		addChild(this, true);
+	}
+
+	private void addChild(Transform child, boolean setParent) {
+		children.add(child);
+		if (setParent) child.setParent(this, false);
+	}
+
+	public Transform getChild(int index) {
+		return children.get(index);
+	}
 
 	/**
 	 * Move the {@link GameObject} that this {@link Transform} is attached to. If the {@link GameObject} has a {@link Collider} attached, then the physics
@@ -20,6 +60,9 @@ public final class Transform extends Component {
 		} else {
 			position.x += x;
 			position.y += y;
+		}
+		for (int i = 0; i < children.size(); i++) {
+			children.get(i).translate(x, y);
 		}
 	}
 }
