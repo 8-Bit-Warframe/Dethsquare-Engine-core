2e86a012cbf182412003479859711e7f02518b7d
Ezard <ezard.ben@gmail.com>
2016-10-25 07:41:52 +0100

    Change from integer network IDs to random strings

--- src/main/java/com/ezardlabs/dethsquare/multiplayer/NetworkBehaviour.java
+++ src/main/java/com/ezardlabs/dethsquare/multiplayer/NetworkBehaviour.java
@@ -3,20 +3,87 @@ package com.ezardlabs.dethsquare.multiplayer;
 import com.ezardlabs.dethsquare.Component;
 
 import java.nio.ByteBuffer;
+import java.util.Random;
 
 public abstract class NetworkBehaviour extends Component {
-	private static int networkIdCounter = 0;
-	int networkId;
+	String networkId;
 	protected int playerId;
 	protected final ByteBuffer data = ByteBuffer.allocate(getSize());
 
 	public NetworkBehaviour() {
-		this(networkIdCounter++, Network.getPlayerID());
+		networkId = generateRandomId();
+		playerId = Network.getPlayerID();
 	}
 
-	NetworkBehaviour(int networkId, int playerId) {
-		this.networkId = networkId;
-		this.playerId = playerId;
+	private String generateRandomId() {
+		char[] chars = {'0',
+				'1',
+				'2',
+				'3',
+				'4',
+				'5',
+				'6',
+				'7',
+				'8',
+				'9',
+				'a',
+				'b',
+				'c',
+				'd',
+				'e',
+				'f',
+				'g',
+				'h',
+				'i',
+				'j',
+				'k',
+				'l',
+				'm',
+				'n',
+				'o',
+				'p',
+				'q',
+				'r',
+				's',
+				't',
+				'u',
+				'v',
+				'w',
+				'x',
+				'y',
+				'z',
+				'A',
+				'B',
+				'C',
+				'D',
+				'E',
+				'F',
+				'G',
+				'H',
+				'I',
+				'J',
+				'K',
+				'L',
+				'M',
+				'N',
+				'O',
+				'P',
+				'Q',
+				'R',
+				'S',
+				'T',
+				'U',
+				'V',
+				'W',
+				'X',
+				'Y',
+				'Z'};
+		Random r = new Random();
+		StringBuilder sb = new StringBuilder(10);
+		for (int i = 0; i < sb.capacity(); i++) {
+			sb.append(chars[r.nextInt(chars.length)]);
+		}
+		return sb.toString();
 	}
 
 	protected abstract ByteBuffer onSend();
