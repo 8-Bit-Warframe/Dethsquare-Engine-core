bbf3d7afa296f4c0a3bed25283a664304a51c0d6
Ezard <ezard.ben@gmail.com>
2016-11-04 15:50:15 +0000

    Change from abstract class to interface

--- src/main/java/com/ezardlabs/dethsquare/prefabs/PrefabCreator.java
+++ src/main/java/com/ezardlabs/dethsquare/prefabs/PrefabCreator.java
@@ -2,6 +2,6 @@ package com.ezardlabs.dethsquare.prefabs;
 
 import com.ezardlabs.dethsquare.GameObject;
 
-public abstract class PrefabCreator {
-	public abstract GameObject create();
+public interface PrefabCreator {
+	GameObject create();
 }
