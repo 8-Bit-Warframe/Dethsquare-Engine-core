5119989509e983f5d12bfdc6cd22f551c62db97f
Ezard <ezard.ben@gmail.com>
2016-11-06 23:21:22 +0000

    Ensure that levels aren't loaded whilst another level load is in progress

--- src/main/java/com/ezardlabs/dethsquare/LevelManager.java
+++ src/main/java/com/ezardlabs/dethsquare/LevelManager.java
@@ -5,12 +5,17 @@ import java.util.HashMap;
 public class LevelManager {
 	private static HashMap<String, Level> levels = new HashMap<>();
 	private static String currentLevelName;
+	private static boolean loadingLevel = false;
 
 	public static void registerLevel(String name, Level level) {
 		levels.put(name, level);
 	}
 
 	public static void loadLevel(String name) {
+		if (loadingLevel) {
+			throw new Error("Cannot load a level whilst another level is being loaded");
+		}
+		loadingLevel = true;
 		currentLevelName = name;
 
 		GameObject.destroyAll();
@@ -21,6 +26,7 @@ public class LevelManager {
 		GameObject.startAll();
 		Renderer.init();
 		Collider.init();
+		loadingLevel = false;
 	}
 
 	public static Level getCurrentLevel() {
