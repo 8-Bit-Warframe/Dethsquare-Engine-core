35b4524bc4267c255634a69673ffb47dbf0d3d6d
Ezard <ezard.ben@gmail.com>
2016-11-16 07:59:00 +0000

    Remove unused method

--- src/main/java/com/ezardlabs/dethsquare/multiplayer/NetworkBehaviour.java
+++ src/main/java/com/ezardlabs/dethsquare/multiplayer/NetworkBehaviour.java
@@ -4,7 +4,6 @@ import com.ezardlabs.dethsquare.Component;
 
 import java.nio.ByteBuffer;
 import java.util.HashMap;
-import java.util.Random;
 
 public abstract class NetworkBehaviour extends Component {
 	static HashMap<Integer, NetworkBehaviour> myNetworkBehaviours = new HashMap<>();
@@ -54,77 +53,6 @@ public abstract class NetworkBehaviour extends Component {
 
 	public int getPlayerId() {
 		return playerId;
-	}
-
-	private String generateRandomId() {
-		char[] chars = {'0',
-				'1',
-				'2',
-				'3',
-				'4',
-				'5',
-				'6',
-				'7',
-				'8',
-				'9',
-				'a',
-				'b',
-				'c',
-				'd',
-				'e',
-				'f',
-				'g',
-				'h',
-				'i',
-				'j',
-				'k',
-				'l',
-				'm',
-				'n',
-				'o',
-				'p',
-				'q',
-				'r',
-				's',
-				't',
-				'u',
-				'v',
-				'w',
-				'x',
-				'y',
-				'z',
-				'A',
-				'B',
-				'C',
-				'D',
-				'E',
-				'F',
-				'G',
-				'H',
-				'I',
-				'J',
-				'K',
-				'L',
-				'M',
-				'N',
-				'O',
-				'P',
-				'Q',
-				'R',
-				'S',
-				'T',
-				'U',
-				'V',
-				'W',
-				'X',
-				'Y',
-				'Z'};
-		Random r = new Random();
-		StringBuilder sb = new StringBuilder(10);
-		for (int i = 0; i < sb.capacity(); i++) {
-			sb.append(chars[r.nextInt(chars.length)]);
-		}
-		return sb.toString();
 	}
 
 	protected abstract ByteBuffer onSend();
